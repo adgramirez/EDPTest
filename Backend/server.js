8b29ed74359a25e7101f579eccfcb48500f62d69
John Kyle Lampa <jklampa31@gmail.com>
2024-03-29 01:36:11 +0800

    change back pw

--- Backend/server.js
+++ Backend/server.js
@@ -9,7 +9,7 @@ app.use(express.json());
 const db = mysql.createConnection({
   host: "localhost",
   user: "root",
-  password: "Fifteen15",
+  password: "AdDU2202201425196",
   database: "edp"
 });
 
